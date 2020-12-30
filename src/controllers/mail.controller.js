d3910a698fc50f1f8bb45af62fa1e8d3cc42e71d
izvolte <50668641+izvolte@users.noreply.github.com>
2020-12-30 15:48:58 +0300

    add mailer

--- src/controllers/mail.controller.js
+++ src/controllers/mail.controller.js
@@ -17,22 +17,20 @@ const sendMail = () => {
 const createUserConfirmationOrderEmail = async ({_id, email}) => {
 
     const mail = await transporter.sendMail({
-        from: `Choose delivery: webmaster2@choosedesign.ru `, // sender address
+        from: `Choose delivery: <webmaster2@choosedesign.ru> `, // sender address
         to: email, // list of receivers
         subject: "Подтверждение заказа", // Subject line
         text: `Ваш заказ с номером ${_id} подтвержден`, // plain text body
         html: "<b>Hello world?</b>", // html body
     });
 
-
-    console.log(mail)
     return mail
 
 }
 const createAdminConfirmationOrderEmail = async ({_id, phone, fullname, address}, email = 'mksepor@gmail.com') => {
 
     const mail = await transporter.sendMail({
-        from: `Choose delivery: webmaster2@choosedesign.ru`, // sender address
+        from: `Choose delivery: <webmaster2@choosedesign.ru>`, // sender address
         to: email, // list of receivers
         subject: `Создан заказ с номером ${_id}`, // Subject line
         text: `Ваш заказ с номером ${_id} подтвержден`, // plain text body
@@ -43,7 +41,6 @@ const createAdminConfirmationOrderEmail = async ({_id, phone, fullname, address}
 `, // html body
     });
 
-    console.log(mail)
     return mail
 
 }
