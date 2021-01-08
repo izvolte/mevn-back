c3f4b403a581f210bd55115eb924252db881513d
izvolte <50668641+izvolte@users.noreply.github.com>
2021-01-08 16:21:46 +0300

    change cat

--- src/middlewares/jwtCheck.middleware.js
+++ src/middlewares/jwtCheck.middleware.js
@@ -6,22 +6,21 @@ const checkJWTSign = async (req, res, next) => {
     const {headers: {authorization}} = req
 
 
+
     if (authorization) {
         const token = authorization.split(' ')[1]
 
         verify(token, process.env.JWT_SECRET, (err) => {
             if (err) {
-                res.sendStatus(402)
+                res.sendStatus(403)
                 return next()
-
             }
-
-            return next()
         })
-
+        return next()
     }
 
-    return res.sendStatus(403)
+
+    res.sendStatus(403)
 }
 
 
