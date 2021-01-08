60c9ad20c1496a163dad3593f682eddc9315767c
izvolte <50668641+izvolte@users.noreply.github.com>
2021-01-08 15:18:24 +0300

    check JWT

--- src/routes/categories.js
+++ src/routes/categories.js
@@ -3,9 +3,9 @@ const checkJWTSign = require('../middlewares/jwtCheck.middleware')
 
 const { category } = require("../controllers")
 
-router.route('/:id').get(checkJWTSign, category.get)
+router.route('/:id').get(category.get)
 router.route('/').post(category.create)
-router.route('/').get(category.getAll)
+router.route('/').get(checkJWTSign, category.getAll)
 router.route('/:id').put(category.update)
 router.route('/:id').delete(category.delete)
 
